b7fc372d4f6f7fe427e7ed558c74a53ede4023c2
Didier <dginebra@altimetrik.com>
2021-10-18 22:33:29 -0400

    before bcrypt

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import User from 'src/persistence/entities/user/user.entity';
+import { UserRepository } from 'src/persistence/repositories/user.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService]
+  imports: [],
+  providers: [UsersService, UserRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import User from "src/persistence/entities/user/user.entity";
+import { UserRepository } from "src/persistence/repositories/user.repository";
+import { UserInputDTO } from "./dto/user.in.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async getByEmail(email: string): Promise<User> {
+    const user: User = await this.userRepository.findOneOrFail({ email });
+    if (user) {
+      return user;
+    }
+    throw new HttpException(
+      "User with this email does not exist",
+      HttpStatus.NOT_FOUND
+    );
+  }
+
+  async createUser(userIn: UserInputDTO): Promise<User> {
+    const userCreated: User = await this.userRepository.create(userIn);
+    await this.userRepository.save(userCreated);
+    return userCreated;
+  }
+}
